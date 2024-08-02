8e68f1e5ddf9b17177eb5dbc271c0befb9328205
DanielSiilva <daniellsousa0614@gmail.com>
2024-08-02 19:59:48 -0300

    refactor: init feature session

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,9 +2,17 @@ import { Express } from "express";
 import { createUserHandler } from "../controller/user.controller";
 import validateResource from "../middleware/validateResource";
 import { createUserSchema } from "../schema/user.schema";
+import { createSessionSchema } from "../schema/session.schema";
+import { createUserSessionHandler } from "../controller/session.controller";
 
 function routes(app: Express) {
   app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+
+  app.post(
+    "/api/sessions",
+    validateResource(createSessionSchema),
+    createUserSessionHandler
+  );
 }
 
 export default routes;
